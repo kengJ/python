827fadcc44f304269dbb74a59088b3df382f1101
kengJ <xheiyu@126.com>
2017-10-31 17:19:07 +0800

    update

--- py3/comm/comm.py
+++ py3/comm/comm.py
@@ -1,10 +1,39 @@
+import os
+
 # 文件处理
 class FileTool:
-    fileUrl = None
-    def __init__(self,fileUrl):
-        self.fileUrl = fileUrl
-    def openFile(self,fileUrl,model):
-        self.file =  open(fileUrl,model)
-    def readAll(self,fileUrl,model,isSetFile=True):
-        # if isSetFile==False:
-        return self.openFile(fileUrl,model).read()
+    def __init__(self):
+        pass
+    def readAllFor(self,file):
+        text = ''
+        with file as f:
+            text+=f.read()
+        return text
+    def readAll(self,fileUrl):
+        file = open(fileUrl,'r')
+        return self.readAllFor(file)
+    def readLines(self,file):
+        return file.readlines()
+    def writeFile(self,fileUrl,message):
+        with open(fileUrl,'w') as file:
+            file.write(message)
+
+
+class osTool:
+    def __init__(self):
+        pass
+    def getSysName(self):
+        osname = os.name
+        if osname == 'nt':
+            print('windows')
+        elif osname == 'posix':
+            print('linux')
+    # linux生效
+    def getSysDetialMessage(self):
+        print(os.uname())
+    # 获取系统变量 path
+    def getPath(self):
+        return os.environ
+    #
+    def getSysVal(self,name):
+        return os.getenv(name)
