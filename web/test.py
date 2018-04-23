0bae11e8a74f49c2997a86e81b16c0d3540b33c4
kengJ <xheiyu@126.com>
2018-04-23 18:24:26 +0800

    update

--- web/test.py
+++ web/test.py
@@ -4,8 +4,13 @@ import platform
 import tool
 import beforeAction as BAction
 import afterAction as AAction
+import win_unicode_console
+
 #show version
 app = Flask(__name__)
+homeIp = '192.168.1.107'
+workIp = '127.0.0.1'
+win_unicode_console.enable()#win10 bug
 
 @app.route('/')
 def index():
@@ -24,16 +29,21 @@ def before_request():
 
 #@after_request 如果有异常抛出将不会被运行
 #@teardown_request 即使有异常抛出也会被执行
-@app.after_request
-def after_request():
-    AAction.main(request.path,None)
+#@app.after_request
+# def after_request():
+#     AAction.main(request.path,None)
 
 @app.route('/test/bbb')
 def test():
     return 'test'
 
+@app.route('/test1')
+def test1():
+    tool.getHostIpAndHostName()
+    return 'test1'
+
 if __name__ == '__main__':
-    app.run(host='192.168.1.107',port=8080,debug=True) 
+    app.run(host=workIp,port=8080,debug=True) 
     print("python version: %s" % platform.python_version())
 
 
--- web/tool.py
+++ web/tool.py
@@ -1,5 +1,11 @@
+import socket
 #返回当前用户IP
 def getUserIp(request):
     return request.remote_addr
 def isPost(request):
     return request.method == 'POST'
+#获取本机IP
+def getHostIpAndHostName():
+    hostname = socket.gethostname()
+    ip = socket.gethostbyname(hostname)
+    print('hostname is %s ip is %s' % (hostname,ip))
